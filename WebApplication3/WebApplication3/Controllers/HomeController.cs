47ec367f1c5222ee720233abf850f62fd32f3bb8
518H0090 <nhoxhieuro5@gmail.com>
2022-04-21 20:46:16 +0700

    setup MVC in ASP.net

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "Huỳnh Trần Trung Hiếu";
+        }
+    }
+}
